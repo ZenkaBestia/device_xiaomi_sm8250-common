2bbb3b1ba147e206eef8d7edf1ae80b6b36d8fdd
Mesquita <mesquita@aospa.co>
2021-05-18 23:08:29 -0300

    sm8250-common: Switch to vibrator qti component
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- kona.mk
+++ kona.mk
@@ -339,7 +339,8 @@ TARGET_COMMON_QTI_COMPONENTS := \
     bt \
     overlay \
     perf \
-    telephony
+    telephony \
+    vibrator
 
 # Rootdir
 PRODUCT_PACKAGES += \
@@ -410,13 +411,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
-
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
