075ec9718443d7a1402e481545d32700abbeb36a
ZenkaBestia <z.mail.6600@gmail.com>
2021-06-17 20:56:41 +0000

    sm8250-common: flag Compile with MSM Kernel in device makefile

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -152,7 +152,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Platform
 BOARD_VENDOR := xiaomi
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_COMPILE_WITH_MSM_KERNEL := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
